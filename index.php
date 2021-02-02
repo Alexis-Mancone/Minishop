f4612153b1703e0d8b51b2bdabf67376dcf337ec
Infernaton <lubin.pauly@gmail.com>
2021-02-02 14:46:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+/*	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/minishop/acceuil_connexion.php');
-	exit;
+	exit;*/
+/* Redirection vers une page différente du même dossier */
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'acceuil_connexion.php';
+header("Location: http://$host$uri/$extra");
+exit;
 ?>
