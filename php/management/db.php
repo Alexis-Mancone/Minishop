d6540e635f6e3a43f86834ec661ff24ae5da0ad0
Infernaton <lubin.pauly@gmail.com>
2021-02-03 16:33:05 +0100

    Link to the dbb

--- php/management/db.php
+++ php/management/db.php
@@ -1,7 +1,13 @@
 <?php
+/*
+https://2eurhost.com/db/
+db: eurh_groupe2
+user: groupe2
+pass: mtNs00_2
+*/
 
 try {
-    $bdd = new PDO('mysql:host=localhost;port=3306;dbname=eurh_groupe2;charset=utf8', "root", "");
+    $bdd = new PDO('mysql:host=2eurhost.com;dbname=eurh_groupe2;port=3306;charset=utf8',"groupe2", "mtNs00_2");
 }
 catch (Exception $e){
     die('Erreur MySQL, veuillez patienter ou contactez un administrateur. <br /><br />' . $e->getMessage());
