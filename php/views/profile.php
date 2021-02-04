d923875c5d5320492dae566c08fd93d08004329a
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-02-04 12:41:16 +0100

    html profil + php profil

--- php/views/profile.php
+++ php/views/profile.php
@@ -7,4 +7,12 @@ var_dump($_SESSION);
 if (strcmp($_SESSION["Pseudo"],"") == 0){
     $page = 'login';
 }
+
+if(isset($_POST['delete'])){
+    $datadelet = $_POST['delete'];
+    $req = $DB->query("DELETE FROM Account WHERE :id_User = id_User",array('id_User' => $datadelet));
+    header('Location: ');
+    $_SESSION['flash']['success'] = "Le compte à été supprimé";
+    exit;
+}
 ?>
